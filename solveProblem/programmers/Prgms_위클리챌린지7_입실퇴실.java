f781efa27b89d6403f61cdb46e23dee613362e36
Hyebeen.So <hyebeen0328@naver.com>
2021-09-17 16:35:36 +0900

    프로그래머스 위클리챌린지7 입실퇴실

--- solveProblem/programmers/Prgms_위클리챌린지7_입실퇴실.java
+++ solveProblem/programmers/Prgms_위클리챌린지7_입실퇴실.java
@@ -14,29 +14,30 @@ public class Prgms_위클리챌린지7_입실퇴실 {
 class Solution {
     public int[] solution(int[] enter, int[] leave) {
         int n = enter.length;
+        int[] answer = new int[n];
+        final int ENTER = 0;
+        final int LEAVE = 1;
 
         int[][] info = new int[n+1][2];
-        for(int i = 0; i < n; ++i) info[enter[i]][0] = i;
-        for(int i = 0; i < n; ++i) info[leave[i]][1] = i;
+        for(int i = 0; i < n; ++i) info[enter[i]][ENTER] = i;
+        for(int i = 0; i < n; ++i) info[leave[i]][LEAVE] = i;
 
         int m = info.length;
         boolean[][] meet = new boolean[m][m];
         for(int idxOfOne = 1 ; idxOfOne < m; ++idxOfOne) {
             int[] one = info[idxOfOne];
-//            for(int idxOfAnother = idxOfOne+1; idxOfAnother < m; ++idxOfAnother) {
             for(int idxOfAnother = 1; idxOfAnother < m; ++idxOfAnother) {
                 int[] another = info[idxOfAnother];
-                if(one[0] < another[0] && another[1] < one[1]) {
+                if(one[ENTER] < another[ENTER] && another[LEAVE] < one[LEAVE]) {
                     meet[idxOfOne][idxOfAnother] = meet[idxOfAnother][idxOfOne] = true;
-                    for(int idxOfBetween = one[0]+1; idxOfBetween < another[0]; ++idxOfBetween) {
+                    for(int idxOfBetween = one[ENTER]+1; idxOfBetween < another[ENTER]; ++idxOfBetween) {
                         int between = enter[idxOfBetween];
-                        if(one[1] < info[between][1]) meet[idxOfOne][between] = meet[between][idxOfOne] = true;
+                        if(one[LEAVE] < info[between][LEAVE]) meet[idxOfOne][between] = meet[between][idxOfOne] = true;
                     }
                 }
             }
         }
 
-        int[] answer = new int[n];
         for(int i = 1; i < m; ++i) {
             for(int j = 1; j < m; ++j) {
                 if(meet[i][j]) answer[i-1]++;
